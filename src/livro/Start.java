617143660950715b4132fa567d1815345cc58c8e
GMusb <musbmg@gmail.com>
2019-08-08 11:50:48 -0300

    Classe livro instânciada

--- src/livro/Start.java
+++ src/livro/Start.java
@@ -3,7 +3,16 @@ package livro;
 public class Start {
 
     public static void main(String[] args) {
-        System.out.println("Deve ser algo com livros");
+        Livro l = new Livro();
+        l.setTitulo("Um Curso Em Milagres");
+        l.setAutor("Helen Schucman");
+        l.setIsbn(1883360749);
+        l.setQtdPaginas(1454);
+        l.setAno(2018);
+        l.setEditora("Foundation for Inner Peace");
+        l.setValor(160,00);
+        l.exibir();
+        l.Venda(200);
     }
     
 }
