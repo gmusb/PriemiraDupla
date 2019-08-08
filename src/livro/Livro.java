557dd55218b86b0c03d6bfc15ace752d62a7785e
GMusb <musbmg@gmail.com>
2019-08-08 11:40:54 -0300

    Classe livro criada

---
+++ src/livro/Livro.java
@@ -0,0 +1,12 @@
+package livro;
+
+
+public class Livro {
+    private String titulo;
+    private String autor;
+    private String isbn;
+    private int qtdPaginas;
+    private int ano;
+    private String editora;
+    private Double valor;
+}
