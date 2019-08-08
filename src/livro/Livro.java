d1c5b8745bbb3a5d1d0f839887c577a3aab70520
GuilhermeJoseSantosdaSilva <gujose00@gmail.com>
2019-08-08 12:07:18 -0300

    Get e Set

--- src/livro/Livro.java
+++ src/livro/Livro.java
@@ -9,4 +9,62 @@ public class Livro {
     private int ano;
     private String editora;
     private Double valor;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getQtdPaginas() {
+        return qtdPaginas;
+    }
+
+    public void setQtdPaginas(int qtdPaginas) {
+        this.qtdPaginas = qtdPaginas;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+    
+    
 }
